db2a156165272a62bd91bb48634a389a5842af60
William Newman <william.newman94@gmail.com>
2019-01-22 19:26:36 -0500

    Added refresh button, refactored a bit

--- src/app/common/services/settings.service.ts
+++ src/app/common/services/settings.service.ts
@@ -8,11 +8,20 @@ export class SettingsService {
   constructor() { }
 
   static get showUsername(): boolean {
-    const _showUsername = window.localStorage.getItem(('showUsername'));
+    const _showUsername = window.localStorage.getItem('showUsername');
     return _showUsername === 'true';
 }
 
   static set showUsername( showUsername: boolean ) {
     window.localStorage.setItem( 'showUsername', String(showUsername));
+  }
+
+  static get showNotifications(): boolean {
+    const _showNotifications = window.localStorage.getItem('showNotifications');
+    return _showNotifications === 'true';
+  }
+
+  static set showNotifications( showNotifications: boolean ) {
+    window.localStorage.setItem( 'showNotifications', String(showNotifications));
   }
 }
