ce743cafd5844f55668f3e0b77490b3bfc6f49b4
William Newman <william.newman94@gmail.com>
2019-02-01 19:06:26 -0500

    Prepping for major change

--- src/app/main-window/main-window.component.ts
+++ src/app/main-window/main-window.component.ts
@@ -20,6 +20,8 @@ import { UsernameService } from '../common/services/username.service';
 import { RawNotification as RawNotificationDto, ThreadNotification } from './dto/raw-notification.dto';
 import { LoadingService } from '../common/services/loading.service';
 import { ChatComponent } from './chat/chat.component';
+import { MessageProcessingService } from '../common/services/message-processing.service';
+import { NotificationProcessingService } from '../common/services/notification-processing.service';
 
 @Component({
   selector: 'app-main-window',
@@ -27,14 +29,14 @@ import { ChatComponent } from './chat/chat.component';
   styleUrls: ['./main-window.component.css'],
 })
 
-export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
+export class MainWindowComponent implements OnInit, OnDestroy {
 
   @ViewChild('container') container: ElementRef;
-
+  @ViewChild(ChatComponent) chat: ChatComponent; // child component so I can trigger scroll to bottom on new message
 
   selectedThread: string;
   messages: ProcessedMessage[] = [];
-  directMessages: ProcessedMessage[];
+  directMessages: ProcessedMessage[] = [];
   threadsSource: Subject<any[]>;
   public threads: any[] = [];
   subscriptions: Subscription[] = [];
@@ -47,7 +49,7 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
   notifications: any[];
   directNotifications: any[];
   users: User[];
-  @ViewChild(ChatComponent) chat: ChatComponent; // child component so I can trigger scroll to bottom on new message
+  isDirectMessage = true;
 
   constructor( private _dataRequestor: DataRequestorService,
     private _messagesService: MessagesService,
@@ -58,8 +60,12 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
     private _notificationsService: NotificationsService,
     private snackbar: MatSnackBar,
     private _loadingService: LoadingService,
-    ) {
-      this.initializeThreads();
+    private _notificationProcessingService: NotificationProcessingService,
+    private _messageProcessingService: MessageProcessingService,
+    ) { }
+
+  ngOnInit() {
+    this.initializeThreads();
       this._messagesService.loadInitialData();
       if ( this.noGroupSelected() ) {
         this.snackbar.open('No Group Selected', 'Notification', {
@@ -69,14 +75,15 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
       }
       this.initializeSubscriptions();
 
-    }
-
-  ngOnInit() {
   }
 
-  ngDoCheck() {
+  ngOnDestroy() {
+    this.leaveAllRooms();
+    this.initializeThreads();
+    this.subscriptions.forEach( sub => sub.unsubscribe());
+    // this._messagesService.disconnect();
+    this._messagesService.loadInitialData();
   }
-
 
   initializeThreads() {
     this._threadService.threadId = null;
@@ -88,21 +95,20 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
   initializeSubscriptions() {
 
     this.onMessagesSubscription = this._messagesService.onMessage().subscribe( responseObject => {
-      this.processSingleMessage( responseObject, false );
+      this.processSingleMessage( responseObject, !this.isDirectMessage );
       this.chat.scrollToBottom();
     });
     this.subscriptions.push( this.onMessagesSubscription );
 
     this.onDirectMessagesSubscription = this._directMessagesService.onMessage().subscribe( responseObject => {
-      this.processSingleMessage( responseObject, true );
+      this.processSingleMessage( responseObject, this.isDirectMessage );
       this.chat.scrollToBottom();
 
     });
     this.subscriptions.push( this.onDirectMessagesSubscription );
 
-
     this.onMessagesJoinSubscription = this._messagesService.onJoin().subscribe( responseObject => {
-      this.processMessagesOnJoin( responseObject, false );
+      this.processMessagesOnJoin( responseObject, !this.isDirectMessage );
       this.chat.scrollToBottom();
 
     });
@@ -110,17 +116,17 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
 
     this.onDirectMessagesJoinSubscription = this._directMessagesService.onJoin().subscribe( responseObject => {
       this._loadingService.isLoading = false;
-      this.processMessagesOnJoin( responseObject, true);
+      this.processMessagesOnJoin( responseObject, this.isDirectMessage);
       this.chat.scrollToBottom();
     });
     this.subscriptions.push(this.onDirectMessagesJoinSubscription);
 
     this.notificationSubscription = this._notificationsService.onMessage().subscribe( res => {
       if  ( res.threadNotifications ) {
-        this.notifications = this.processNotifications( res.threadNotifications );
+        this.notifications = this._notificationProcessingService.processNotifications( res.threadNotifications );
       }
       if ( res.directThreadNotifications ) {
-        this.directNotifications = this.processNotifications( res.directThreadNotifications);
+        this.directNotifications = this._notificationProcessingService.processNotifications( res.directThreadNotifications);
       }
     });
     this.subscriptions.push(this.notificationSubscription);
@@ -130,26 +136,26 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
 
   processSingleMessage( responseObject: any, isDirect: boolean ) {
     this.users = responseObject.users;
-    if (this.messages.length > 0 || this.directMessages.length > 0) {
-      if ( isDirect ) {
+    if ( this.messages.length > 0 || this.directMessages.length > 0 ) {
+      if ( isDirect && this.directMessages.length > 0 ) { // if direct msg and direct mes
         const lastMsg = this.directMessages[this.directMessages.length - 1];
-        const newMsg = this.processMessages([lastMsg, responseObject.message]); // Process new message with last message
+        const newWithLast =  [lastMsg, responseObject.message];
+        const newMsg = this._messageProcessingService.processMessages(newWithLast, this.users); // Process new message with last message
         this.directMessages = this.directMessages.concat( newMsg.slice(2) ); // Remove last Message
-      } else {
+      } else if ( !isDirect && this.messages.length > 0 ) {
         const lastMsg = this.messages[this.messages.length - 1]; // Get Last Message so can see if time divider is needed
-        const newMsg = this.processMessages([lastMsg, responseObject.message]); // Process new message with last message
+        const newWithLast =  [lastMsg, responseObject.message];
+        const newMsg = this._messageProcessingService.processMessages( newWithLast, this.users ); // Process new message with last message
         this.messages = this.messages.concat( newMsg.slice(2) ); // Remove last Message
       }
-
     } else {
-      const newMsg = this.processMessages([responseObject.message]);
+      const newMsg = this._messageProcessingService.processMessages([responseObject.message], this.users);
       if ( isDirect ) {
         this.directMessages = newMsg;
       } else {
         this.messages = newMsg;
       }
     }
-
     if ( isDirect ) {
       this._notificationsService.readDirect( GroupService.id, this._directThreadService.threadId, UsernameService.id );
     } else {
@@ -162,25 +168,19 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
     this._loadingService.isLoading = false;
 
     if ( isDirect ) {
-      this.directMessages = this.processMessages( responseObject.messages );
+      this.directMessages = this._messageProcessingService.processMessages( responseObject.messages, this.users );
       this._notificationsService.readDirect( GroupService.id, this._directThreadService.threadId, UsernameService.id );
     } else {
-      this.messages = this.processMessages( responseObject.messages );
+      this.messages = this._messageProcessingService.processMessages( responseObject.messages, this.users );
       this._notificationsService.read( GroupService.id, this._threadService.threadId, UsernameService.id );
     }
   }
 
   noGroupSelected(): boolean {
+
     return GroupService.group === '';
   }
 
-  ngOnDestroy() {
-    this.leaveAllRooms();
-    this.initializeThreads();
-    this.subscriptions.forEach( sub => sub.unsubscribe());
-    // this._messagesService.disconnect();
-    this._messagesService.loadInitialData();
-  }
 
   leaveAllRooms() {
     if ( this._threadService.threadId ) {
@@ -199,10 +199,6 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
     this._loadingService.isLoading = false;
   }
 
-  scrollToBottom() {
-    this.container.nativeElement.scrollTop = this.container.nativeElement.scrollHeight;
-  }
-
   getMessages() {
     if ( this._threadService.threadId !== null ) {
 
@@ -215,92 +211,4 @@ export class MainWindowComponent implements OnInit, OnDestroy, DoCheck {
       return [];
     }
   }
-
-
-  processMessages( messages: Message[]): ProcessedMessage[] {
-    const processedMessages: ProcessedMessage[] = [];
-    let previousTime: number;
-    for ( const message of messages ) {
-      if ( message === undefined ) {
-        continue; // skip one iteration
-      }
-      message.createdAt = new Date(message.createdAt);
-      const user: User = this.matchMessageUserIdWithUser( message );
-      if ( previousTime === undefined || this.isPastNextDay( previousTime, ( message.createdAt.getTime() / 1000 ) ) ) {
-        processedMessages.push( this.createTimeDivider( message ) );
-      }
-      const processedMessage = new ProcessedMessage(undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined);
-      processedMessage.setValues( message, user );
-      processedMessages.push( processedMessage );
-      previousTime = message.createdAt.getTime() / 1000;
-    }
-
-    return processedMessages;
-  }
-
-  private createTimeDivider( message ) {
-    const date: Date = new Date(message.createdAt);
-    date.setHours( 0, 0, 0, 0);
-    return new ProcessedMessage(undefined, undefined, undefined, undefined, undefined, undefined, date, true );
-
-  }
-
-  private matchMessageUserIdWithUser( message: Message ): User {
-    for ( const user of this.users ) {
-      if ( user.id === message.userId ) {
-        return user;
-      }
-    }
-  }
-
-  private isPastNextDay( previousTime: number, newTime: number ): boolean {
-    // const roundedPreviousTime: number = previousTime + ( 3600 * 24 - (previousTime % (3600 * 24)) );
-    const roundedPreviousTime: number = previousTime + ( (3600 * 24) - (previousTime % (3600 * 24) ) );
-
-    return newTime > roundedPreviousTime;
-
-  }
-
-  private roundDown( newTime: number ): number {
-
-    return newTime - ( newTime % (3600 * 24) );
-  }
-
-  private processNotifications( rawNotificationDto: any ): ProcessedNotification[] {
-    const processedNotifications = [];
-    for ( const threadNotification of rawNotificationDto ) {
-      const notificationCount: number = this.findNotificationCount( threadNotification );
-      const threadId: number = threadNotification.threadId;
-      const processed: ProcessedNotification = new ProcessedNotification( threadId, notificationCount );
-      processedNotifications.push( processed );
-    }
-
-    return processedNotifications.sort( ( a: ProcessedNotification, b: ProcessedNotification ) => {
-        if ( a.threadId > b.threadId ) {
-          return 1;
-        } else if ( a.threadId < b.threadId ) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-  }
-
-  private findNotificationCount( threadNotification: ThreadNotification ) {
-    let notificationCount = 0;
-    for ( const message of threadNotification.strippedDownMessages ) {
-      if ( this.userInIds(message) ) {
-        notificationCount += 1;
-      }
-    }
-
-    return notificationCount;
-  }
-
-  private userInIds( message: any ) {
-    const index = message.userIds.indexOf( Number(UsernameService.id) );
-
-    return index === -1;
-  }
-
 }
